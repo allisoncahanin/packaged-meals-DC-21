0558eb841bb925a689f36146791893728c73a0b5
allisoncahanin <79371214+allisoncahanin@users.noreply.github.com>
2021-02-23 10:44:43 -0500

    Add/clean food names
    Used inner_join to add the food names from food.csv and tidied names using regex to remove things like oz/pack at the end of names.

--- meal_nutrients_DC_21.R
+++ meal_nutrients_DC_21.R
@@ -3,32 +3,48 @@ library(tidytext)
 
 meals_nutrient <- read.csv("C:/Users/allis/OneDrive/Documents/GitHub/packaged-meals-DC-21/food_nutrient.csv", header = T)
 nutrient_names <- read.csv("C:/Users/allis/OneDrive/Documents/GitHub/packaged-meals-DC-21/nutrient.csv")
+food_names <- read.csv("C:/Users/allis/OneDrive/Documents/GitHub/packaged-meals-DC-21/food.csv")
+
 
 id_list <- meals_wide_df$fdc_id
 id_list
 
-meals_nutrient$id <- meals_nutrient$derivation_id <- NULL
+meals_nutrient$id <- meals_nutrient$derivation_id <- NULL #removing unnecessary columns
 
-nutrient_subset <- meals_nutrient[meals_nutrient$fdc_id %in% id_list,]
+nutrient_subset <- meals_nutrient[meals_nutrient$fdc_id %in% id_list,] #create a subset of nutritional values using id_list
 
-nutrient_subset$data_points <- nutrient_subset$min <- nutrient_subset$max <- NULL
+nutrient_subset$data_points <- nutrient_subset$min <- nutrient_subset$max <- NULL #removing unnecessary columns
 nutrient_subset$median <- nutrient_subset$footnote <- nutrient_subset$min_year_acquired <- NULL
 
-nutrient_subset <- nutrient_subset %>%
+nutrient_subset <- nutrient_subset %>% #join nutrient names to data frame using nutrient codes
   inner_join(nutrient_names, by = c("nutrient_id" = "id")) %>%
   select(name, everything(nutrient_subset))
 
-nutrient_subset$nutrient_id <- NULL
+nutrient_subset$nutrient_id <- NULL #removing unnecessary column
 
-nutrient_wide_df <- spread(nutrient_subset, key = name, value = amount, fill = 0)
-nutrient_wide_df <- data_frame(nutrient_wide_df)
+nutrient_wide_df <- spread(nutrient_subset, key = name, value = amount, fill = 0) #pivot data frame wider and fill empty cells with 0
+nutrient_wide_df <- data_frame(nutrient_wide_df) 
+nutrient_wide_df[is.na(nutrient_wide_df)] <- 0 #change any NA values to 0
 
-nutrient_wide_df[is.na(nutrient_wide_df)] <- 0
 
-
-nutrient_wide_df <- nutrient_wide_df %>%
+nutrient_wide_df <- nutrient_wide_df %>% #adding brand and food category
   inner_join(meals) %>%
   select(branded_food_category, brand_owner, everything(nutrient_wide_df))
+
+nutrient_wide_df <- nutrient_wide_df %>% #adding food description (name of food)
+  inner_join(food_names) %>%
+  select(description, everything(nutrient_wide_df))
+
+nutrient_wide_df <- nutrient_wide_df %>% #text cleaning with regular expressions to remove oz/lb and pack/count
+  mutate(description = str_to_title(description),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}\\.?[[:alnum:]]{0,2}[[:blank:]]?oz", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}\\.?[[:alnum:]]{0,2}[[:blank:]]?ounce", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}[[:blank:]]?ct", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}[[:blank:]]?count", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}[[:blank:]]?pack", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}[[:blank:]]?pk", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]{1,3}[[:blank:]]?lb", ignore_case = T)),
+         description = str_remove(description, regex(" [[:alnum:]]/[[:alnum:]][[:blank:]]?lb", ignore_case = T)))
 
 
 ################################################
@@ -37,15 +53,15 @@ nutrient_wide_df <- nutrient_wide_df %>%
 
 library(tidymodels)
 
-pca_rec <- recipe(~ ., data = nutrient_wide_df) %>% #no values computed here
-  update_role(fdc_id, branded_food_category, brand_owner, new_role = "id") %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors())
+nutrient_pca_rec <- recipe(~ ., data = nutrient_wide_df) %>% #create recipe for PCA
+  update_role(fdc_id, branded_food_category, brand_owner, description, new_role = "id") %>% #set roles as "id" for non-predictor variables
+  step_normalize(all_predictors()) %>% #center and scale all predictors
+  step_pca(all_predictors()) #train PCA (no values computed here)
 
-pca_prep <- prep(pca_rec) #values are computed
-pca_prep
+nutrient_pca_prep <- prep(nutrient_pca_rec) #values are computed
+nutrient_pca_prep
 
-nutrient_tidied_pca <- tidy(pca_prep, 2)
+nutrient_tidied_pca <- tidy(nutrient_pca_prep, 2)
 
 nutrient_tidied_pca %>% 
   filter(component %in% paste0("PC", 1:5)) %>%
@@ -67,27 +83,31 @@ nutrient_tidied_pca %>%
   facet_wrap(~component, scales = "free_y") +
   labs(y = NULL, fill = "Positive?")
 
-juice(pca_prep) %>%
-  ggplot(aes(PC1, PC3, label = fdc_id)) +
-  geom_point(aes(color = branded_food_category),alpha = 0.7, size = 2) +
+juice(nutrient_pca_prep) %>%
+  ggplot(aes(PC1, PC2, label = description)) +
+  geom_point(aes(color = branded_food_category),alpha = 0.7, size = 1.5) +
   geom_text(check_overlap = TRUE, hjust = "inward", family = "IBMPlexSans") +
   labs(color = NULL)
 
-install.packages("embed")
+################################################
+###             EXPLORATORY UMAP             ###
+################################################
+
 library(embed)
 
-umap_rec <- recipe(~ ., data = nutrient_wide_df) %>% #no values computed here
-  update_role(fdc_id, branded_food_category, brand_owner, new_role = "id") %>%
-  step_normalize(all_predictors()) %>%
-  step_umap(all_predictors())
+#UMAP is a different model based on topology that looks at clustering
 
-umap_prep <- prep(umap_rec) #values are computed
-umap_prep
+nutrient_umap_rec <- recipe(~ ., data = nutrient_wide_df) %>% #create recipe for UMAP
+  update_role(fdc_id, branded_food_category, brand_owner, description, new_role = "id") %>% #set id roles for non-predictors
+  step_normalize(all_predictors()) %>% #center and scale predictors
+  step_umap(all_predictors()) #train UMAP (no values computed here)
 
-juice(umap_prep) %>% #different model based on clustering
-  ggplot(aes(umap_1, umap_2, label = fdc_id)) +
-  geom_point(aes(color = branded_food_category),alpha = 0.7, size = 2) +
+nutrient_umap_prep <- prep(nutrient_umap_rec) #values are computed
+nutrient_umap_prep
+
+juice(nutrient_umap_prep) %>%
+  ggplot(aes(umap_1, umap_2, label = description)) +
+  geom_point(aes(color = branded_food_category),alpha = 0.7, size = 1.5) +
   geom_text(check_overlap = TRUE, hjust = "inward", family = "IBMPlexSans") +
   labs(color = NULL)
 
-#need to do inner join to add names of food from "food" csv using the fdc_id to match
